8d383d6e7cba8e095198ec20b865cd619a86de91
Jae-Hong-Kim <newfly101@naver.com>
2025-02-24 20:59:33 +0900

    [servlet] query parameter을 쉽게 읽을수 있는 function 지원

---
+++ src/main/java/com/daybreak/SpringMVCDemo/basic/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.daybreak.SpringMVCDemo.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+@WebServlet(name="helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("req = " + req);
+        System.out.println("res = " + res);
+
+        // query parameter 을 쉽게 읽을 수 있는 getParameter함수 지원
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+    }
+}
