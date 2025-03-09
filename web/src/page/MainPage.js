a14c22dbe3f4c773e5612777c9db3f0119360798
Jae-Hong-Kim <newfly101@naver.com>
2025-03-09 21:05:25 +0900

    [FE] CSS 추가 및 설정 변경

--- web/src/page/MainPage.js
+++ web/src/page/MainPage.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {Link} from "react-router";
+import {useNavigate} from "react-router";
+import styles from "../css/global/index.scss";
 
 const MainPage = () => {
+    const navigate = useNavigate();
+    const handleNavigateTo = (url) => {
+        navigate(url)
+    }
     return (
-        <ul>
-            <li><Link to="/basic">서블릿 basic</Link></li>
-        </ul>
+        <>
+            <h3 className={styles.headlineHover}>서블릿 베이직</h3>
+            <button className={styles.linkTag} onClick={()=> handleNavigateTo("/basic")}>
+                서블릿 basic
+            </button>
+        </>
     );
 };
 
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,7 +1,27 @@
 module.exports = {
     devServer: {
-        proxy:{
-            '/':"http://localhost:3080",
+        proxy: {
+            '/': "http://localhost:3080",
         }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader",
+                    "resolve-url-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass"), // 최신 Dart Sass 사용
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
     }
-}
+};
