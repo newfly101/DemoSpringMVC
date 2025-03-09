25bfcaf58699971550d1b00b9a2d9facd187c755
Jae-Hong-Kim <newfly101@naver.com>
2025-03-09 20:40:59 +0900

    [Spring] HttpServletParams > params 생성

--- src/main/java/com/daybreak/SpringMVCDemo/basic/request/RequestParamServlet.java
+++ src/main/java/com/daybreak/SpringMVCDemo/basic/request/RequestParamServlet.java
@@ -1,7 +1,26 @@
 package com.daybreak.SpringMVCDemo.basic.request;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ *
+ */
+@WebServlet(name="requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
 
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("RequestParamServlet.service");
+
+        request.getParameterNames();
+    }
 }
