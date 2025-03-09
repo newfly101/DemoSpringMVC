050cb76226f6f8bf93fb816a96411d6db2e45f12
Jae-Hong-Kim <newfly101@naver.com>
2025-03-09 21:30:55 +0900

    [FE] FE 디자인 변경

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,7 @@
 import MainPage from "./page/MainPage";
 import React from "react";
-import {BrowserRouter as Router, Routes, Route, useNavigate} from "react-router";
+import {BrowserRouter as Router, Routes, Route} from "react-router";
 import BasicPage from "./page/BasicPage";
-import RequestHeader from "./page/components/RequestHeader";
 
 function App() {
     return (
@@ -10,7 +9,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainPage/>}/>
                 <Route path="/basic" element={<BasicPage/>}/>
-                <Route path="/request-header" element={<RequestHeader/>}/>
             </Routes>
         </Router>
     );
