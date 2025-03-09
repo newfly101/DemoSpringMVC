5ae8f9dc1dc99e35dbd953ca1af0b923b45d9f2d
Jae-Hong-Kim <newfly101@naver.com>
2025-03-09 21:05:09 +0900

    [FE] 파일 삭제 및 재정립

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
 import MainPage from "./page/MainPage";
 import React from "react";
-import {BrowserRouter as Router, Routes, Route } from "react-router";
+import {BrowserRouter as Router, Routes, Route, useNavigate} from "react-router";
 import BasicPage from "./page/BasicPage";
+import RequestHeader from "./page/components/RequestHeader";
 
 function App() {
-  return (
-    <Router>
-        <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/basic" element={<BasicPage />} />
-        </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainPage/>}/>
+                <Route path="/basic" element={<BasicPage/>}/>
+                <Route path="/request-header" element={<RequestHeader/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
